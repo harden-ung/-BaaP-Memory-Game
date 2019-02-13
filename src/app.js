160fafe4a5aea9369c2af0525031c3f3fb70aa1c
Trung Ung <ungkientrung@gmail.com>
2019-02-13 11:14:48 +0200

    problem that one card is set not to have iconClass

---
+++ src/app.js
@@ -0,0 +1,213 @@
+// app.js
+'use strict';
+console.log("app starting...");
+//IIFE so that one cannot access variables from Chrome Inspect
+(()=>{
+    //declair constants 
+    const CONST = {
+        BOARD_SIZE: 12,
+        CARDS_PER_ROW: 3,
+        CARD_INVISIBLE: "oi-aperture",
+        GAME_STATE: {
+            NO_TURNED_CARD: 0,
+            ONE_TURNED_CARD: 1,
+            TWO_TURNED_CARDS: 2,
+            GAME_OVER: 3
+        },
+        CARD_STATE: {
+            IN_GAME: 0,
+            OPENED: 1
+        },
+        TURN_INVISIBLE_DELAY: 700
+    }
+
+    //class MemoryCard to create card objects
+    class MemoryCard {
+        constructor(id, gameController) {
+            this.id = id;
+            this.element = document.getElementById("card-" + id);
+            this.span = this.element.getElementsByTagName("span")[0];
+            this.gameController = gameController;
+            this.iconClass = "";
+            this.state = CONST.CARD_STATE.IN_GAME;
+            //one trick offent used in React to help bind this to object of class MemoryCard instead of this to event object
+            this.onClickhandler = this.onClickhandler.bind(this);
+        }
+        onClickhandler(e) {
+            if (this.gameController.state === CONST.GAME_STATE.TWO_TURNED_CARDS) return;
+            if (this.state === CONST.CARD_STATE.IN_GAME) {
+                this.gameController.turnCard(this.id);
+                console.log(this);
+                console.log("card " + this.id + " clicked");
+            }
+        }
+        turnVisible() {
+            this.span.classList.remove(CONST.CARD_INVISIBLE);
+            this.span.classList.add(this.iconClass);
+            this.element.classList.add("animated", "flipInX");
+            this.state = CONST.CARD_STATE.OPENED;
+        }
+        turnInvisible() {
+            this.element.classList.remove("flipInX");
+            this.element.classList.add("flipOutX");
+            this.promise = new Promise((resolve, reject) => {
+                this.element.addEventListener("animationend", () => {
+                    this.span.classList.remove(this.iconClass);
+                    this.element.classList.remove("animated", "flipOutX")
+                    this.span.classList.add(CONST.CARD_INVISIBLE);
+                    resolve();
+                }, { once: true });
+            });
+            this.state = CONST.CARD_STATE.IN_GAME;
+        }
+        getIcon() {
+            return this.iconClass;
+        }
+        setIcon(x) {
+            this.iconClass = ICONNAMES[x];
+        }
+    }
+
+    class MemoryGame {
+        constructor(size, cardsPerRow) {
+            this.nbrOfCards = size;
+            this.cardsPerRow = cardsPerRow;
+            this.cards = [];
+            this.firstCard = undefined;
+            this.secondCard = undefined;
+            this.state = CONST.GAME_STATE.NO_TURNED_CARD;
+            this.moveCount = 0;
+            this.timePlay = -1;
+        }
+        inittialize() {
+            this.createDivs();
+            this.setEventListeners();
+            this.setIconClassToCards();
+        }
+        setPlayTime(){
+            if(this.state === CONST.GAME_STATE.GAME_OVER) return;
+            setInterval(() => {
+                document.getElementById("play-time").innerText = `Playtime: ${++this.timePlay} s`
+            }, 1000);
+        }
+        setIconClassToCards() {
+            let i, icon, x, y;
+            for (i = 0; i < this.nbrOfCards / 2; i++) {
+                icon = Math.floor(Math.random() * ICONNAMES.length);
+                x = Math.floor(Math.random() * this.nbrOfCards);
+                y = Math.floor(Math.random() * this.nbrOfCards);
+
+                x = this.getNextUninitializedIconClassIndex(x);
+                y = this.getNextUninitializedIconClassIndex(y);
+
+                this.cards[x].setIcon(i);
+                this.cards[y].setIcon(i);
+            }
+        }
+        getNextUninitializedIconClassIndex(x) {
+            for (let i = 0; i < this.nbrOfCards; i++) {
+                let n = (x + i) % this.nbrOfCards;
+                if (this.cards[n].getIcon() === "")
+                    return n;
+            }
+            throw ("Error");//should not reach
+        }
+        setEventListeners() {
+            for (let i = 0; i < this.nbrOfCards; i++) {
+                this.cards[i] = new MemoryCard(i, this);
+                this.cards[i].element.addEventListener("click", this.cards[i].onClickhandler);
+            }
+        }
+        createRow(id) {
+            let divRow = document.createElement("div");
+            divRow.id = "row-" + id;
+            divRow.classList.add("row");
+            return divRow;
+        }
+        createCard(id) {
+            let divCard = document.createElement("div");
+            divCard.id = "card-" + id;
+            divCard.classList.add("col-sm" , "card");
+            return divCard;
+        }
+        createCardBody() {
+            let divCardBody = document.createElement("div");
+            divCardBody.classList.add("card-body");
+            return divCardBody;
+        }
+        createIcon(id) {
+            let iconSpan = document.createElement("span");
+            iconSpan.id = "span-" + id;
+            iconSpan.classList.add("oi", "text-primary");
+            iconSpan.classList.add(CONST.CARD_INVISIBLE);
+            return iconSpan;
+        }
+        createDivs() {
+            let i, j;
+            let cardId = 0;
+            let rowElement, cardElement, cardBodyElement, iconElement;
+            for (i = 0; i < this.nbrOfCards / this.cardsPerRow; i++) {
+                rowElement = this.createRow(i);
+                for (j = 0; j < this.cardsPerRow; j++) {
+                    cardId = j + i * this.cardsPerRow;
+                    cardElement = this.createCard(cardId);
+                    cardBodyElement = this.createCardBody();
+                    iconElement = this.createIcon(cardId);
+                    cardBodyElement.appendChild(iconElement);
+                    cardElement.appendChild(cardBodyElement);
+                    rowElement.appendChild(cardElement);
+                }
+                document.getElementById("game-content").appendChild(rowElement);
+            }
+        }
+        turnCard(id) {
+            if (this.timePlay === -1){
+                this.timePlay = 0;
+                this.setPlayTime();
+            }
+            switch (this.state) {
+                case CONST.GAME_STATE.NO_TURNED_CARD:
+                    this.firstCard = this.cards[id];
+                    this.firstCard.turnVisible();
+                    this.state = CONST.GAME_STATE.ONE_TURNED_CARD;
+                    break;
+                case CONST.GAME_STATE.ONE_TURNED_CARD:
+                    if (this.id === this.firstCard.id) break;
+                    this.secondCard = this.cards[id];
+                    this.secondCard.turnVisible();
+
+                    this.state = CONST.GAME_STATE.TWO_TURNED_CARDS;
+                    //move counter by +1
+                    document.getElementById("turn-count").innerText = ++this.moveCount;
+
+                    //if cards does not match
+                    if (this.firstCard.getIcon() !== this.secondCard.getIcon()) {
+                        setTimeout(() => {
+                            this.firstCard.turnInvisible();
+                            this.secondCard.turnInvisible();
+                            Promise.all([this.firstCard.promise, this.secondCard.promise]).then(() => {
+                                this.state = CONST.GAME_STATE.NO_TURNED_CARD;
+                                this.firstCard.promise = new Promise(() => 0);
+                                this.secondCard.promise = new Promise(() => 0);
+                            })
+                        }, CONST.TURN_INVISIBLE_DELAY);
+                    } else { //cards match
+                        this.firstCard.element.classList.add("card-done");
+                        this.secondCard.element.classList.add("card-done");
+                        this.firstCard.element.removeEventListener("click", this.firstCard.onClickhandler);
+                        this.secondCard.element.removeEventListener("click", this.secondCard.onClickhandler);
+                        this.state = CONST.GAME_STATE.NO_TURNED_CARD;
+                    }
+                    break;
+                case CONST.GAME_STATE.TWO_TURNED_CARDS:
+                    break
+                default:
+                    break;
+            }
+        }
+    }
+
+    let memoryGame = new MemoryGame(CONST.BOARD_SIZE, CONST.CARDS_PER_ROW);
+
+    memoryGame.inittialize();
+})()
---
+++ src/icons.js
@@ -0,0 +1,74 @@
+// icons.js
+
+ICONNAMES = [
+  'oi-ban',
+  'oi-bell',
+  'oi-briefcase',
+  'oi-bug',
+  'oi-heart',
+  'oi-camera-slr',
+  'oi-cog',
+  'oi-command',
+  'oi-envelope-closed',
+  'oi-headphones',
+  'oi-heart',
+  'oi-image',
+  'oi-key',
+  'oi-lock-locked',
+  'oi-map-marker',
+  'oi-medical-cross',
+  'oi-microphone',
+  'oi-moon',
+  'oi-musical-note',
+  'oi-phone',
+  'oi-rain',
+  'oi-rss',
+  'oi-sun',
+  'oi-shield',
+  'oi-tablet',
+  'oi-target',
+  'oi-trash',
+  'oi-volume-high',
+  'oi-wifi',
+  'oi-wrench',
+  'oi-x'
+];
+
+// icons.js
+
+// ICONNAMES = [
+//   'text-primary oi oi-ban',
+//   'text-primary oi oi-bell',
+//   'text-primary oi oi-briefcase',
+//   'text-primary oi oi-bug',
+//   'text-primary oi oi-heart',
+//   'text-primary oi oi-camera-slr',
+//   'text-primary oi oi-cog',
+//   'text-primary oi oi-command',
+//   'text-primary oi oi-envelope-closed',
+//   'text-primary oi oi-headphones',
+//   'text-primary oi oi-heart',
+//   'text-primary oi oi-image',
+//   'text-primary oi oi-key',
+//   'text-primary oi oi-lock-locked',
+//   'text-primary oi oi-map-marker',
+//   'text-primary oi oi-medical-cross',
+//   'text-primary oi oi-microphone',
+//   'text-primary oi oi-moon',
+//   'text-primary oi oi-musical-note',
+//   'text-primary oi oi-phone',
+//   'text-primary oi oi-rain',
+//   'text-primary oi oi-rss',
+//   'text-primary oi oi-sun',
+//   'text-primary oi oi-shield',
+//   'text-primary oi oi-tablet',
+//   'text-primary oi oi-target',
+//   'text-primary oi oi-trash',
+//   'text-primary oi oi-volume-high',
+//   'text-primary oi oi-wifi',
+//   'text-primary oi oi-wrench',
+//   'text-primary oi oi-x'
+// ];
+
+
+
