3c360f34237102be103d5e6edc0541c2584b6495
Trung Ung <ungkientrung@gmail.com>
2019-02-17 18:17:45 +0200

    ready to submmit

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ console.log("app starting...");
 (()=>{
     // declair constants 
     const CONST = {
-        BOARD_SIZE: 4,
-        CARDS_PER_ROW: 2,
+        BOARD_SIZE: 16,
+        CARDS_PER_ROW: 4,
         CARD_INVISIBLE: "oi-aperture",
         GAME_STATE: {
             NO_TURNED_CARD: 0,
@@ -76,58 +76,84 @@ console.log("app starting...");
             this.state = CONST.GAME_STATE.NO_TURNED_CARD;
             this.moveCount = 0;
             this.secondPlay = -1;
-            this.timePlay = "";
             this.foundPairs = 0;
             this.playTimeInterval;
-            //this.playDate;
         }
         inittialize() {
+            this.addDefaultHighScore();
             this.createDivs();
             this.setEventListeners();
             this.setIconClassToCards();
         }
+        addDefaultHighScore(){
+            //add 3 default highscore
+            if(localStorage.getItem("highscore") === null){     
+                localStorage.setItem("highscore", JSON.stringify([{ turn: this.nbrOfCards / 2 + 5, time: 20 }, { turn: this.nbrOfCards / 2 + 10, time: 36 }, { turn: this.nbrOfCards / 2 + 13, time: 30 }]));
+            }
+        }
         gameEnd(){
             console.log("Game End");
             clearInterval(this.playTimeInterval);      
-            
-            // set play score
-            document.getElementById("yourScore").innerText = `You finish the game with ${this.moveCount} moves in ${this.timePlay}.`;
-
+            // show play score
+            document.getElementById("yourScore").innerText = `You finish the game with ${this.moveCount} moves in ${this.toMinuteAndSecond(this.secondPlay)}.`;
+            this.setHighscore();
+            this.showHighScore();
+            this.showModal();
+        }
+        setHighscore(){
+            //assign temporary score object
+            let tempScoreObject = { turn: this.moveCount, time: this.secondPlay };
+            //assign savedScoreArray
+            let savedScoreArray = JSON.parse(localStorage.getItem("highscore"));
+            savedScoreArray.push(tempScoreObject);
+            //set highscore
+            //sort array by turn and time
+            let newHighscoreArray = savedScoreArray.sort((arr, cur) => {
+                //loop through arr and curent value, swap if arr turn < cur turn
+                if (arr.turn < cur.turn) return -1;
+                if (arr.turn > cur.turn) return 1;
+                // if arr.turn === cur.turn, swap if arr.time < cur.time
+                if (arr.time < cur.time) return -1;
+                if (arr.time > cur.time) return 1;
+            }).slice(0, 3);
+            localStorage.setItem("highscore", JSON.stringify(newHighscoreArray));
+        }
+        showHighScore(){
+            //show highscore         
+            document.getElementById("highScore1").innerHTML = `Batman \n ${JSON.parse(localStorage.getItem("highscore"))[0].turn} moves ${this.toMinuteAndSecond(JSON.parse(localStorage.getItem("highscore"))[0].time)}`;
+            document.getElementById("highScore2").innerHTML = `Joker ${JSON.parse(localStorage.getItem("highscore"))[1].turn} moves ${this.toMinuteAndSecond(JSON.parse(localStorage.getItem("highscore"))[1].time)}`;
+            document.getElementById("highScore3").innerHTML = `Halley Quinn ${JSON.parse(localStorage.getItem("highscore"))[2].turn} moves ${this.toMinuteAndSecond(JSON.parse(localStorage.getItem("highscore"))[2].time)}`;
+        }
+        showModal(){
             //display modal
             let modalElement = document.getElementById("highScoreModal");
-            modalElement.style.display = "block";    
+            modalElement.style.display = "block";
             //add listener for buttons
             let closeBtn = document.getElementById("closeModal");
             let replayBtn = document.getElementById("replay");
-            closeBtn.addEventListener("click",()=>{
+            closeBtn.addEventListener("click", () => {
                 modalElement.style.display = "none";
             })
-            replayBtn.addEventListener("click",()=>{
+            replayBtn.addEventListener("click", () => {
                 modalElement.style.display = "none";
                 location.reload();
             })
         }
+        toMinuteAndSecond(second){
+            let minute = Math.floor(second / 60);
+            second -= minute*60;
+            if (minute === 0){
+                return `${second}s`;
+            } else return `${minute}'${second}s`
+
+        }
         setPlayTime(){
             if(this.state === CONST.GAME_STATE.GAME_OVER) return;
             //use arrowFunction here so that this here is MemoryGame object. Otherwise, it will be HTML document.getElementById object
-            let m = 0;
-            this.playTimeInterval = setInterval(() => {
-                let timeString = "";
-                if (this.secondPlay === 60){
-                    m++;
-                    this.secondPlay = 0;
-                }
-                if (m === 0 ){
-                    timeString = `Playtime: ${++this.secondPlay}s`;
-                    this.timePlay = `${++this.secondPlay}s`;
-                } 
-                else {
-                    timeString = `Playtime: ${m}' ${++this.secondPlay}s`;
-                    this.timePlay = `${m}'${++this.secondPlay}s`;
-                }    
-                
-                document.getElementById("play-time").innerText = timeString
-            }, 100);
+            this.playTimeInterval = setInterval(() => {         
+                this.secondPlay++;  
+                document.getElementById("play-time").innerText = `Playtime: ${this.toMinuteAndSecond(this.secondPlay)}`;               
+            }, 1000);
         }
         setIconClassToCards() {
             let x, y;
@@ -217,7 +243,7 @@ console.log("app starting...");
 
                     this.state = CONST.GAME_STATE.TWO_TURNED_CARDS;
                     //move counter by +1
-                    document.getElementById("turn-count").innerText = ++this.moveCount;
+                    document.getElementById("turn-count").innerText = `Turns: ${++this.moveCount}`;
 
                     //if cards does not match
                     if (this.firstCard.getIcon() !== this.secondCard.getIcon()) {
